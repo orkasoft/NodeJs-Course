826009ad0dfb71ae0e7134ea7d6f00fe3310f56f
emreordukaya <emreordukaya@hotmail.com>
2024-03-02 10:02:17 +0300

    guncelle

--- Chepter 2 Samples/bolum_tekrar_1.js
+++ Chepter 2 Samples/bolum_tekrar_1.js
@@ -1,5 +1,9 @@
 https://www.yazilimkodlama.com/sql-server-2/sql-calisma-sorulari/
 
+https://mt-prime-win.guzelhosting.com:8443/phpMyAdmin/index.php?db=ordukay1_deneme
+Kullanıcı: ordukay1_deneme
+Sifre: 7A12j!io9
+
 
 //Soru1 Klavyeden girilen 3 sayının ortalamasını alan bir uygulama yazınız
 
